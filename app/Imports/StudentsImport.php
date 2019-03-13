323e996be0c5712f028e768fc6682569fdfbff9d
cckaron <cg.workst@gmail.com>
2019-03-13 18:55:07 +0800

    add backup

--- app/Imports/StudentsImport.php
+++ app/Imports/StudentsImport.php
@@ -3,23 +3,39 @@
 namespace App\Imports;
 
 use App\Student;
-use Maatwebsite\Excel\Concerns\ToModel;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Storage;
+use Maatwebsite\Excel\Concerns\ToCollection;
 
-class StudentsImport implements ToModel
+class StudentsImport implements ToCollection
 {
     /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
-    public function model(array $row)
+     * @param Collection $collection
+     * @return void
+     */
+    public function collection(Collection $collection)
     {
-        return new Student([
-            'users_id' => $row[0],
-            'users_name' => $row[1],
-            'department' => $row[2],
-            'grade' => $row[3],
-            'class' => $row[4],
-        ]);
+        foreach ($collection as $row) {
+//            return new Student([
+//                'users_id' => $row[0],
+//                'users_name' => $row[1],
+//                'department' => $row[2],
+//                'grade' => $row[3],
+//                'class' => $row[4],
+//            ]);
+
+            Student::firstOrCreate([
+                'users_id' => $row[0],
+                'users_name' => $row[1],
+                'department' => $row[2],
+                'grade' => $row[3],
+                'class' => $row[4],
+            ]);
+
+            Storage::makeDirectory('public/'.$row[0]);
+            Storage::disk('public')->put(
+                $row[0].'/init.txt', 'init'
+            );
+        }
     }
 }
