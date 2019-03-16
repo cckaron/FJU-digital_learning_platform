28c7d7519b275e4baef902422c8de8af063af3bc
cckaron <cg.workst@gmail.com>
2019-03-17 00:46:49 +0800

    add backup log

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
         //          ->hourly();
         $schedule
             ->command('backup:run')
-            ->everyFiveMinutes()
+            ->everyTenMinutes()
             ->withoutOverlapping()
             ->appendOutputTo(storage_path('logs/backup.log'));;
 
